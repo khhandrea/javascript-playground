bc28283b5054892329ca23a4b16e72af92ee71cc
khhandrea <khhandrea@naver.com>
2020-12-14 19:13:07 +0900

    Add word relay script

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-const body = document.querySelector("body");
+let body = document.querySelector(`body`),
+    wordBox = body.querySelector(`.wordBox`),
+    word = body.querySelector(`.word`),
+    form = body.querySelector(`form`),
+    input = body.querySelector(`input`),
+    button = body.querySelector(`button`),
+    result = body.querySelector(`.result`);
+
+let lastWord = word.textContent;
+
+form.addEventListener(`submit`, (event) => {
+    event.preventDefault();
+    if (lastWord[lastWord.length - 1] === input.value[0]) { // correct
+        result.textContent = `correct!`;
+        result.classList.remove(`wrong`);
+        newWord = document.createElement(`div`);
+        newWord.textContent = input.value;
+        wordBox.append(newWord);
+        lastWord = input.value;
+        input.value = ``;
+        input.focus();
+    } else { // wrong
+        input.value = ``;
+        input.focus();
+        result.textContent = `wrong!`;
+        result.classList.add(`wrong`);
+    }
+});
+
+function init() {
+
+}
+
+init();
